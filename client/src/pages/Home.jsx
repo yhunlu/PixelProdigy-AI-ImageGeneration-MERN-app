8c20067ed032a3edd82e1d531556b4275e60c03a
yhunlu <yhunlu87@gmail.com>
2023-01-30 03:03:15 +0300

    added utils getrandomprompt, create-post page.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from '../components';
 
 const RenderCards = ({ data, title }) => {
-  if (data?.lenght > 0)
+  if (data?.lenght > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
+  }
 
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
@@ -15,7 +16,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setSearchText] = useState(null);
+  const [searchText, setSearchText] = useState('');
 
   return (
     <section className="max-w-7xl mx-auto">
