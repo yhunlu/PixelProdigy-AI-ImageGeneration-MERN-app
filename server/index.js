5ac27b2fdefb709e681d2eee0444c9d6f742e160
yhunlu <yhunlu87@gmail.com>
2023-02-01 17:43:13 +0300

    added connectDB utils, implemented to backend.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect';
 
 dotenv.config();
 
@@ -13,9 +14,14 @@ app.get('/', async (req, res) => {
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log('Server has started on port http://localhost:8080')
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log('Server has started on port http://localhost:8080')
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
